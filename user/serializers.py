1fd2638087b9a039ef41d54d014c5916ae93a1b3
m3 <sergey.radik@gmail.com>
2026-06-13 17:34:49 +0300

    fix. make CustomAuthTokenSerializer accept both email and username fields optionally

--- user/serializers.py
+++ user/serializers.py
@@ -26,7 +26,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class CustomAuthTokenSerializer(serializers.Serializer):
-    username = serializers.CharField(label=_("Email"), write_only=True)
+    email = serializers.EmailField(label=_("Email"), required=False, write_only=True)
+    username = serializers.CharField(label=_("Username"), required=False, write_only=True)
     password = serializers.CharField(
         label=_("Password"),
         style={"input_type": "password"},
@@ -36,7 +37,8 @@ class CustomAuthTokenSerializer(serializers.Serializer):
     token = serializers.CharField(label=_("Token"), read_only=True)
 
     def validate(self, attrs):
-        email = attrs.get("username")
+        # Приймаємо будь-який ключ, який надіслали тести або DRF
+        email = attrs.get("email") or attrs.get("username")
         password = attrs.get("password")
 
         if email and password:
@@ -50,7 +52,7 @@ class CustomAuthTokenSerializer(serializers.Serializer):
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
         else:
-            msg = _('Must include "email" and "password".')
+            msg = _('Must include "email" or "username" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
 
         attrs["user"] = user
