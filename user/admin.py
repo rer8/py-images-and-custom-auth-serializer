9b316546d9ce6ec779dfd0c0e010a657f9aea2c9
m3 <sergey.radik@gmail.com>
2026-06-13 17:28:28 +0300

    fix. remove username field and apply user model migrations

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User, UserAdmin)
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password"),
+            },
+        ),
+    )
